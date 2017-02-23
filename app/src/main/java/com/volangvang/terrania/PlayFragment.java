525a8a1d1acc4c6875ed1ef8dda95c3f983d8ec0
Phạm Nguyễn Hoàng <pnhoang96@gmail.com>
2017-02-23 23:56:16 +0700

    Initial commit Initialise the project and add fragment stubs

---
+++ app/src/main/java/com/volangvang/terrania/PlayFragment.java
@@ -0,0 +1,37 @@
+package com.volangvang.terrania;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class PlayFragment extends Fragment {
+
+    public PlayFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_play, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getActivity().setTitle(R.string.nav_play);
+    }
+}
