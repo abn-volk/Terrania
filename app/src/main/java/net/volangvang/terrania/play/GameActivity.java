f5ba7a61bef6bb72d2923ecaf107fbd04a1280b9
Phạm Nguyễn Hoàng <pnhoang96@gmail.com>
2017-05-17 23:04:10 +0700

    Set background and update launcher icon

--- app/src/main/java/net/volangvang/terrania/play/GameActivity.java
+++ app/src/main/java/net/volangvang/terrania/play/GameActivity.java
@@ -5,11 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -42,7 +42,7 @@ public class GameActivity extends AppCompatActivity implements GoogleApiClient.C
     public static final String EXTRA_CONTINENT = "continent";
     public static final String TAG = "server_fragment";
     @BindView(R.id.fragment_holder)
-    FrameLayout holder;
+    CoordinatorLayout holder;
     private String mode;
     private ServerFragment fragment;
     private Server server;
